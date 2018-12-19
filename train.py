1be71c0848c02adacce4dfa82ca3c8eb9a5d53f7
Mitch <mitchyc@hotmail.com.au>
2018-12-19 13:10:28 +1000

    Added commenting.

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import scipy.io
 from PIL import Image
 from tqdm import tqdm
 
+# Training resource URL/file names
 TRAIN_DATA_URL = 'http://imagenet.stanford.edu/internal/car196/cars_train.tgz'
 TRAIN_DEVKIT_URL = 'https://ai.stanford.edu/~jkrause/cars/car_devkit.tgz'
 
@@ -17,6 +18,7 @@ TRAIN_DEVKIT_FILE_NAME = TRAIN_DEVKIT_URL.split('/')[-1]
 
 
 def download_file(url, chunk_size=1024, progress_bar=True):
+    # Download a file from the specified URL
     fname = url.split('/')[-1]
     fpath = './data/{}'.format(fname)
     r = requests.get(url, stream=True)
@@ -33,19 +35,22 @@ def download_file(url, chunk_size=1024, progress_bar=True):
 
 
 def extract_tgz(fname, dest):
+    # Extract the given tgz file
+    if fname.split('.')[-1] is not 'tgz':
+        return
     tar = tarfile.open(fname, "r:gz")
     tar.extractall(dest)
     tar.close()
 
 
 def get_relative_position(p1, p2, size):
-    try:
-        return (p1[0] / size[0], p1[1] / size[1]), (p2[0] / size[0], p2[1] / size[1])
-    except (IndexError, ZeroDivisionError):
-        return None
+    # Return the pixel percentage of the given points respective of the image size
+    return (p1[0] / size[0], p1[1] / size[1]), (p2[0] / size[0], p2[1] / size[1])
 
 
 def resize_image(im, size=(800, 600)):
+    # Resize an image (default is 800x600)
+    # Crops and pads an image while maintaining original aspect ratio
     aspect = im.size[0] / im.size[1]
     im = im.resize((size[0], int(size[0] / aspect)), Image.ANTIALIAS)
     diff_y = size[1] - im.size[1]
@@ -59,55 +64,69 @@ def resize_image(im, size=(800, 600)):
     return im
 
 
-if not os.path.isdir('./data'):
-    os.mkdir('data')
+if __name__ == '__main__':
+    if not os.path.isdir('./data'):
+        os.mkdir('data')
 
-if not os.path.isdir('./data/devkit/') or not os.listdir('./data/devkit/'):
-    if not os.path.isfile('./data/{}'.format(TRAIN_DEVKIT_FILE_NAME)):
-        print('Retrieving training annotations...')
-        download_file(TRAIN_DEVKIT_URL, chunk_size=32, progress_bar=False)
-    print('Extracting annotations...')
-    extract_tgz('./data/{}'.format(TRAIN_DEVKIT_FILE_NAME), './data/')
+    if not os.path.isdir('./data/devkit/') or not os.listdir('./data/devkit/'):
+        if not os.path.isfile('./data/{}'.format(TRAIN_DEVKIT_FILE_NAME)):
+            # Download devkit archive
+            print('Retrieving training annotations...')
+            download_file(TRAIN_DEVKIT_URL, chunk_size=32, progress_bar=False)
+        # Extract devkit training annotations
+        print('Extracting annotations...')
+        extract_tgz('./data/{}'.format(TRAIN_DEVKIT_FILE_NAME), './data/')
 
-if not os.path.isdir('./data/cars_train/') or not os.listdir('./data/cars_train/'):
-    if not os.path.isfile('./data/{}'.format(TRAIN_DATA_FILE_NAME)):
-        print('Retrieving training data...')
-        download_file(TRAIN_DATA_URL)
-    print('Extracting images...')
-    extract_tgz('./data/{}'.format(TRAIN_DATA_FILE_NAME), './data/')
+    if not os.path.isdir('./data/cars_train/') or not os.listdir('./data/cars_train/'):
+        if not os.path.isfile('./data/{}'.format(TRAIN_DATA_FILE_NAME)):
+            # Download training dataset
+            print('Retrieving training data...')
+            download_file(TRAIN_DATA_URL)
+        # Extract training images
+        print('Extracting images...')
+        extract_tgz('./data/{}'.format(TRAIN_DATA_FILE_NAME), './data/')
 
-train_annos = scipy.io.loadmat('./data/devkit/cars_train_annos.mat')['annotations']
-classes = scipy.io.loadmat('./data/devkit/cars_meta.mat')
+    # Load training annotations and car classes
+    train_annos = scipy.io.loadmat('./data/devkit/cars_train_annos.mat')['annotations']
+    classes = scipy.io.loadmat('./data/devkit/cars_meta.mat')
 
-train_data = list()
-train_labels = list()
+    train_data = list()
+    train_labels = list()
 
-tmp_dir = './tmp/'
+    tmp_dir = './tmp/'
 
-if not os.path.isdir(tmp_dir):
-    os.mkdir('tmp')
+    if not os.path.isdir(tmp_dir):
+        os.mkdir('tmp')
 
-for ann in tqdm(train_annos[0], desc='Processing training images', file=sys.stdout, unit='images'):
-    img_name = ann[5][0]
-    im = None
+    # Load/process images in training set
+    for ann in tqdm(train_annos[0], desc='Processing training images', file=sys.stdout, unit='images'):
+        img_name = ann[5][0]
+        im = None
 
-    if os.path.isfile('{}{}'.format(tmp_dir, img_name)):
-        im = Image.open('{}{}'.format(tmp_dir, img_name))
-    else:
-        im = Image.open('./data/cars_train/{}'.format(img_name))
-        im = resize_image(im)
-        im.save('{}{}'.format(tmp_dir, img_name))
+        if os.path.isfile('{}{}'.format(tmp_dir, img_name)):
+            # Load processed image from tmp_dir
+            im = Image.open('{}{}'.format(tmp_dir, img_name))
+        else:
+            # Load raw image, resize and save processed image
+            im = Image.open('./data/cars_train/{}'.format(img_name))
+            im = resize_image(im)
+            im.save('{}{}'.format(tmp_dir, img_name))
 
-    # train_data.append(im)
-    p1, p2 = (int(ann[0][0]), int(ann[1][0])), (int(ann[2][0]), int(ann[3][0]))
-    label = get_relative_position(p1, p2, im.size)
-    if label is not None:
-        train_labels.append(label)
+        # train_data.append(im)
 
-train_data = np.array(train_data)
+        # Convert bounding box pixel points to relative points
+        p1, p2 = (int(ann[0][0]), int(ann[1][0])), (int(ann[2][0]), int(ann[3][0]))
+        label = get_relative_position(p1, p2, im.size)
+        if label is not None:
+            train_labels.append(label)
 
-aug = Augmentor.Pipeline(tmp_dir, output_directory='aug')
-aug.rotate(0.3, 8, 8)
-aug.skew_left_right(0.4, 0.3)
-aug.flip_left_right(0.5)
-aug.sample(100)
+    train_data = np.array(train_data)
+
+    # Add processed images to augmentation pipeline and apply operations
+    aug = Augmentor.Pipeline(tmp_dir, output_directory='aug')
+    aug.rotate(0.3, 8, 8)
+    aug.skew_left_right(0.4, 0.3)
+    aug.flip_left_right(0.5)
+
+    # Save augmented images to './{tmp_dir}/aug'
+    aug.sample(100)
