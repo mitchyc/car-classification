d873ecc10cf4c3308adfb611795dcb1e9c7183ad
Mitch <mitchyc@hotmail.com.au>
2019-02-05 22:37:58 +1000

    Added additional layers to nn, bounding box overlay for eval results

--- predict.py
+++ predict.py
@@ -3,8 +3,11 @@ import os
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-
 from setup import resize_image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+np.set_printoptions(suppress=True)
 
 EVAL_DIR = 'eval'
 
@@ -15,23 +18,37 @@ HEIGHT = 60
 WIDTH = 80
 CHANNELS = 1
 
-if __name__ == '__main__':
-    if not os.path.isdir(EVAL_DIR):
-        os.mkdir(EVAL_DIR)
+THRESHOLD = 0
 
+if __name__ == '__main__':
     eval_files = os.listdir(EVAL_DIR)
     file_count = len(eval_files)
     eval_data = np.empty(shape=(file_count, HEIGHT, WIDTH, CHANNELS))
+    orig_data = [None] * file_count
     i = 0
     for file in eval_files:
         im = Image.open(os.path.join(EVAL_DIR, file))
+        orig_data[i] = im
         im = im.convert('L')
         im, _ = resize_image(im, (WIDTH, HEIGHT), (0, 0, 0, 0))
-        im = np.array(im)
+        im = np.array(im) / 255.0
         im = np.expand_dims(im, -1)
         eval_data[i] = im
         i += 1
 
     model = tf.keras.models.load_model(MODEL_FILE)
     predictions = model.predict(eval_data)
-    print(predictions)
+    del model
+
+    for i in range(file_count):
+        if predictions[i][0] > THRESHOLD:
+            p0 = predictions[i][2] * orig_data[i].size[0], predictions[i][3] * orig_data[i].size[1]
+            box_size = (predictions[i][4] - predictions[i][2]) * orig_data[i].size[0], (
+                    predictions[i][5] - predictions[i][3]) * orig_data[i].size[1]
+            fig, ax = plt.subplots(1)
+            ax.imshow(orig_data[i])
+            box = patches.Rectangle(p0, box_size[0], box_size[1], linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(box)
+            plt.show()
+
+            print('{}'.format(predictions[i]))
--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import tensorflow as tf
 
+tf.enable_eager_execution()
+
 DATA_DIR = 'data'
 TRAIN_DATA_DIR = os.path.join(DATA_DIR, 'train')
 TRAIN_DATA = 'train_data.npy'
@@ -14,20 +16,29 @@ TEST_LABELS = 'test_labels.npy'
 MODEL_DIR = 'model'
 MODEL_FILE = os.path.join(MODEL_DIR, 'model.h5')
 
+BATCH_SIZE = 32
+
 
 if __name__ == '__main__':
-    train_data = np.load(os.path.join(TRAIN_DATA_DIR, TRAIN_DATA))
+    train_data = np.load(os.path.join(TRAIN_DATA_DIR, TRAIN_DATA)) / 255.0
     train_labels = np.load(os.path.join(TRAIN_DATA_DIR, TRAIN_LABELS))
 
-    test_data = np.load(os.path.join(TEST_DATA_DIR, TEST_DATA))
+    test_data = np.load(os.path.join(TEST_DATA_DIR, TEST_DATA)) / 255.0
     test_labels = np.load(os.path.join(TEST_DATA_DIR, TEST_LABELS))
 
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv2D(192, kernel_size=3, activation='relu', input_shape=(60, 80, 1)))
-    model.add(tf.keras.layers.Conv2D(256, kernel_size=7, activation='relu'))
+    model.add(tf.keras.layers.Conv2D(64, kernel_size=3, activation='relu', input_shape=(60, 80, 1)))
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=3, activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=3))
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=2, activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=2, activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(6, activation='softmax'))
+    model.add(tf.keras.layers.Dense(128))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(6))
 
     model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-    model.fit(train_data, train_labels, epochs=5, validation_data=(test_data, test_labels))
+    model.fit(train_data, train_labels, batch_size=BATCH_SIZE, epochs=1, validation_data=(test_data, test_labels))
     model.save(MODEL_FILE)
